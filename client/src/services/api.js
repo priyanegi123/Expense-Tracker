21e9bbd031d7af98413bae3abeffabb551873b97
priyanegi123 <priyanegi230704@gmail.com>
2026-06-07 14:17:12 +0530

    update api url for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000";
+const API_BASE = "https://expense-tracker-api-p8hs.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE,
